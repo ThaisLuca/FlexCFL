d0e220070181ff400d5dd6d1f098631da33f2b5c
Thais Luca <thais.luca95@gmail.com>
2022-07-05 19:27:56 +0000

    Seting MNIST as dataset

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from flearn.trainer.fedavg import FedAvg
 from flearn.trainer.fedgroup import FedGroup
 
 def main():
-    config = TrainConfig('femnist', 'mlp', 'fedavg')
+    config = TrainConfig('mnist', 'mlp', 'fedavg')
     config.trainer_config['dynamic'] = False
     trainer = FedAvg(config)
     trainer.train()
     #trainer.train_locally()
 
 def main_flexcfl():
-    config = TrainConfig('femnist', 'mlp', 'fedgroup')
+    config = TrainConfig('mnist', 'mlp', 'fedgroup')
     config.trainer_config['dynamic'] = True
     config.trainer_config['shift_type'] = "all"
     config.trainer_config['swap_p'] = 0.05
